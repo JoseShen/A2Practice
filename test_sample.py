f20fff90410f0aa939f959ec2bb6725205d9288f
Jose Shen <joseshen999@gmail.com>
2022-03-11 15:12:38 -0500

    added anoter test

--- test_sample.py
+++ test_sample.py
@@ -7,6 +7,7 @@ class TestFoo(unittest.TestCase):
     self.assertEqual(foo(1), 3)
     self.assertEqual(foo(2), 4)
     self.assertEqual(foo(3), 5)
+    self.assertEqual(foo(4), 6)
 
 if __name__ == "__main__":
   unittest.main()
